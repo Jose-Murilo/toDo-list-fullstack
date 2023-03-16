50e4ef279ec89b3d835c34a759f40eaa3b3628d3
Jose Murilo <murilojucas360@hotmail.com>
2023-03-16 16:00:12 -0300

    add const middleware

---
+++ server/src/middlewares/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+const { secret } = require('../configs/auth.json')
+
+module.exports = (req, res) => {
+    const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json("Please inform your token");
+
+    jwt.verify(token, secret, (error, decode) => {
+        if (error) return res.status(401).json(error);
+        req.userID = decode.id;
+        next();
+    })
+};
--- server/src/routes/register.js
+++ server/src/routes/register.js
@@ -2,11 +2,23 @@ const { Router } = require('express');
 const router = Router();
 const registerControllers = require('../controllers/register');
 const loginControllers  = require('../controllers/auth');
+const authMiddleware = require('../middlewares/auth');
+const prisma = require('../database');
 
 router.get('/register', registerControllers.read);
 router.post('/register', registerControllers.create);
 
 router.post('/login', loginControllers.auth);
 
+router.get('/system', authMiddleware, async (req, res) => {
+    const id = req.userID;
+    const user = await prisma.users.findUnique({where: {id: Number(id)}});
+    res.json({
+        system: true, 
+        id,
+        email: user.email, 
+        status: "authorized"
+    })
+});
 
 module.exports = router
