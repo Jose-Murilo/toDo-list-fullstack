4542e577688c5181e71c69548c2859c1cedf4cc9
Jose Murilo <murilojucas360@hotmail.com>
2023-03-16 15:46:19 -0300

    add login system jwt

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,3 +1,26 @@
 const prisma = require('../database');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { secret, expiresIn } = require('../configs/auth.json')
+
+module.exports = {
+    async auth(req, res) {
+        const { email, password } = req.body;
+        if (!email) return res.status(400).json('Email is required');
+        if (!password) return res.status(400).json('Password is required');
+
+        const user = await prisma.register.findUnique({where: {email}});
+        if (!user) return res.status(400).json('Email or Password is required');
+
+        const passwordCheck = await bcrypt.compare(password, user.password);
+        if (!passwordCheck) return res.status(400).json('Password or Email is required');
+
+        const token = jwt.sign({ id: user.id, secret, expiresIn })
+
+        return res.status(200).json({
+            id: user.id,
+            status: 'AuAuthenticated',
+            token
+        })
+    }
+}
--- server/src/routes/register.js
+++ server/src/routes/register.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 const registerControllers = require('../controllers/register');
+const loginControllers  = require('../controllers/auth');
 
 router.get('/register', registerControllers.read);
 router.post('/register', registerControllers.create);
 
-router.post('/login')
+router.post('/login', loginControllers.auth);
+
 
 module.exports = router
